43a08706db19f40cb5b67d613fcddf4f755edf30
Vishal Sharma <Vishal.Sharma@npadfhsc.com.au>
2020-02-12 14:55:00 +1000

    initial commit decode jwt token .net core console app

---
+++ CSharpUtils/jwt-token/JwtTokenDecoder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace JwtTokenDecoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jwtToken = @"<put your token here instead of this placeholder>";
+
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.ReadJwtToken(jwtToken);
+
+            Console.WriteLine(token);
+            Console.WriteLine("Decoced JWT Token is above in cmd window. Once you found the value you were looking from decoded token. Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
